da7451154055ff3e7da4058f0acac034639fbbe1
Luke Hickin <luke.hickin@shopify.com>
2024-04-11 15:40:48 -0700

    Add oh-my-zsh install script

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 
 # Pre-reqs
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
